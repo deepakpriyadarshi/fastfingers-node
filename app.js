0e74aacff826757beae47070e9695f60251d8b19
Deepak Priyadarshi <connect@deepakpriyadarshi.com>
2021-03-02 16:46:42 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const playerRouter = require('./routers/player.router');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/fastfingers/api/player', playerRouter);
+
+app.get('/fastfingers/api', (request, response) => {
+    response.json({
+        status: 'success',
+        message: 'This is Fastfingers Node API ',
+    });
+});
+
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server Running On ${process.env.APP_PORT}`);
+});
